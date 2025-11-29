729f25afbabbaa059eadb3f21023a0bcadb921f5
nites <nitescucornelionut19@gmail.com>
2025-11-29 20:48:49 +0200

    setup

---
+++ Shared.Infra/Services/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Freelance.Shared.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Infra.Services;
+
+public class UnitOfWork<T> : IUnitOfWork<T> where T : DbContext
+{
+    private readonly T _context;
+
+    public UnitOfWork(T context)
+    {
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
+        _context = context;
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
