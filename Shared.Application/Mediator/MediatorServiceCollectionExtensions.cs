7e3b9008538ba90290b1ddfb239755dbc281cb50
nites <nitescucornelionut19@gmail.com>
2025-12-20 13:32:09 +0200

    change mediator injection

--- Shared.Application/Mediator/MediatorServiceCollectionExtensions.cs
+++ Shared.Application/Mediator/MediatorServiceCollectionExtensions.cs
@@ -1,42 +1,50 @@
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Application.Behaviours;
 using System.Reflection;
 
-namespace Shared.Application.Mediator;
-
-/// <summary>
-/// Extension methods to register mediator services.
-/// </summary>
-public static class MediatorServiceCollectionExtensions
+namespace Shared.Application.Mediator
 {
     /// <summary>
-    /// Registers the mediator, request handlers, and pipeline behaviors.
+    /// Provides extension methods for registering MediatR-style mediator services and pipeline behaviors.
     /// </summary>
-    public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+    public static class MediatorServiceCollectionExtensions
     {
-        services.AddScoped<IMediator, Mediator>();
+        /// <summary>
+        /// Registers the <see cref="IMediator"/> implementation, request handlers, and pipeline behaviors
+        /// from the specified assemblies.
+        /// </summary>
+        /// <param name="services">The service collection to add services to.</param>
+        /// <param name="assemblies">Assemblies to scan for request handlers and pipeline behaviors.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddScoped<IMediator, Mediator>();
 
-        var handlerTypes = assemblies
-            .SelectMany(a => a.GetTypes())
-            .Where(t => t.IsClass && !t.IsAbstract)
-            .SelectMany(t => t.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
-                .Select(i => new { Interface = i, Implementation = t }))
-            .ToList();
+            var handlerTypes = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
+                    .Select(i => new { Interface = i, Implementation = t }))
+                .ToList();
 
-        foreach (var handler in handlerTypes)
-            services.AddScoped(handler.Interface, handler.Implementation);
+            foreach (var handler in handlerTypes)
+                services.AddScoped(handler.Interface, handler.Implementation);
 
-        var behaviorTypes = assemblies
-            .SelectMany(a => a.GetTypes())
-            .Where(t => t.IsClass && !t.IsAbstract)
-            .SelectMany(t => t.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>))
-                .Select(i => new { Interface = i, Implementation = t }))
-            .ToList();
+            var behaviorTypes = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>))
+                    .Select(i => new { Interface = i, Implementation = t }))
+                .ToList();
 
-        foreach (var behavior in behaviorTypes)
-            services.AddScoped(behavior.Interface, behavior.Implementation);
+            foreach (var behavior in behaviorTypes)
+                services.AddScoped(behavior.Interface, behavior.Implementation);
 
-        return services;
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
     }
 }
