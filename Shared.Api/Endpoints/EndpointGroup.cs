f38a8fc446ad56dbd5d4ba78efbda6f732b66b0f
ionut1912 <nitescucornelionut19@gmail.com>
2026-04-04 09:24:56 +0300

    fix

--- Shared.Api/Endpoints/EndpointGroup.cs
+++ Shared.Api/Endpoints/EndpointGroup.cs
@@ -2,6 +2,7 @@
 
 namespace Shared.Api.Endpoints;
 
+
 /// <summary>
 ///     Provides a base class for defining endpoint groups in the web API.
 ///     Implementations should override <see cref="Map" /> to register endpoints with the application's routing system.
